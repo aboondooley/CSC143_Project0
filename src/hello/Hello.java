e5b118202a941b79319e055053d4a199b7f4cbdd
Alie Boon-Dooley <asbd@uw.edu>
2019-09-27 21:46:31 -0700

    Filled in all of the methods and constructors in Hello.java

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -3,7 +3,7 @@ package hello;
 public class Hello {
     /* YOUR CODE HERE */
     // field
-    private String name;
+    private String greetMe;
 
     public static void main(String[] args) {
         Hello hello = new Hello();
@@ -16,19 +16,20 @@ public class Hello {
 
     // Consutructor
     public Hello() {
-        // I am not sure about this one
+      String greetMe = "World";
     }
 
     //Constructor
     public Hello(String inputName) {
-        String name = inputName;
+        String greetMe = inputName;
     }
 
     public String greeting() {
         /* YOUR CODE HERE */
+        return "Hello, " + greetMe + "!";
     }
 
     public String toString() {
-        /* YOUR CODE HERE */
+        return greeting();
     }
 }
